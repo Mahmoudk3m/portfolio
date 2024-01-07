6e6d94995d96f4c00ce20f63585f72b7b9db95c3
MahmoudK3m <mahmoudk3m.mk@gmail.com>
2024-01-08 00:53:57 +0200

    small fixes

--- src/lib/components/Sections/Projects.svelte
+++ src/lib/components/Sections/Projects.svelte
@@ -2,7 +2,7 @@
 	import Card from '../Projects/Card.svelte';
 </script>
 
-<section class="flex flex-col items-center self-center m-2">
+<section class="flex flex-col items-center self-center m-4">
 	<h1 class="text-4xl w-fit m-1">Until we arrive... Since you asked... Here's my past...</h1>
 	<div class="flex flex-col items-center mt-4 max-w-3xl" style="gap:16px;">
 		<Card
