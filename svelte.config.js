5e88b474e961cbf3e840145c48ada9d55d9d9aa9
Mahmoudk3m <mahmoudk3m.mk@gmail.com>
2024-04-11 09:22:56 +0200

    Update GitHub Pages deployment workflow and Svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
